b4b670667176a48069cd5034e17ad4db18a06efb
Richard Willis <willis.rh@gmail.com>
2015-02-20 17:43:14 +0000

    Simplified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,36 +10,16 @@ var watchify = require('watchify');
 var es6ify = require('es6ify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
-
-/** Config variables */
-var serverPort = 8888;
-var lrPort = 35731;
-
-/** File paths */
-var dist = 'dist';
-
-var htmlFiles = 'app/**/*.html';
-var htmlBuild = dist;
+var serveStatic = require('serve-static');
+var config = require('./config.json');
 
 var jsxFiles = 'app/jsx/**/*.jsx';
-
 var vendorFiles = [
   'bower_components/react/react-with-addons.js',
   'node_modules/es6ify/node_modules/traceur/bin/traceur-runtime.js'
 ];
-
-var vendorBuild = dist + '/vendor';
+var vendorBuild = config.distPath + '/vendor';
 var requireFiles = './node_modules/react/react.js';
-
-gulp.task('vendor', function () {
-  return gulp.src(vendorFiles).
-    pipe(gulp.dest(vendorBuild));
-});
-
-gulp.task('html', function () {
-  return gulp.src(htmlFiles).
-    pipe(gulp.dest(htmlBuild));
-});
 
 function compileScripts(watch) {
   gutil.log('Starting browserify');
@@ -47,67 +27,60 @@ function compileScripts(watch) {
   var entryFile = './app/jsx/app.jsx';
   es6ify.traceurOverrides = {experimental: true};
 
-  var bundler;
+  var bundler = browserify({
+    cache: {},
+    packageCache: {},
+    fullPaths: true,
+    debug: true
+  });
+  bundler.add(entryFile);
   if (watch) {
-      bundler = watchify(entryFile);
-  } else {
-      bundler = browserify(entryFile);
+    bundler = watchify(bundler);
   }
-
   bundler.require(requireFiles);
   bundler.transform(reactify);
   bundler.transform(es6ify.configure(/.jsx/));
 
   var rebundle = function () {
-    var stream = bundler.bundle({ debug: true});
+    var stream = bundler.bundle();
 
     stream.on('error', function (err) { console.error(err) });
     stream = stream.pipe(source(entryFile));
     stream.pipe(rename('app.js'));
-    stream.pipe(gulp.dest('dist/bundle'));
+    stream.pipe(gulp.dest(config.distPath+'/bundle'));
   }
 
   bundler.on('update', rebundle);
   return rebundle();
 }
 
+gulp.task('vendor', function () {
+  return gulp.src(vendorFiles)
+    .pipe(gulp.dest(vendorBuild));
+});
+
+gulp.task('html', function () {
+  return gulp.src(htmlFiles)
+    .pipe(gulp.dest(config.distPath));
+});
 
 gulp.task('server', function (next) {
   connect()
-  .use(connect.static(dist))
-  .listen(serverPort, function() {
-    gutil.log('Server listening on port:', serverPort);
+  .use(serveStatic(config.distPath))
+  .listen(config.serverPort, function() {
+    gutil.log('Server listening on port:', config.serverPort);
     next();
   });
 });
 
-
-function initWatch(files, task) {
-  if (typeof task === "string") {
-    gulp.start(task);
-    gulp.watch(files, [task]);
-  } else {
-    task.map(function (t) { gulp.start(t) });
-    gulp.watch(files, task);
-  }
-}
-
-/**
- * Run default task
- */
-gulp.task('default', ['vendor', 'server'], function () {
-  var lrServer = livereload(lrPort);
-  var reloadPage = function (evt) {
-      lrServer.changed(evt.path);
-  };
-
-  function initWatch(files, task) {
-      gulp.start(task);
-      gulp.watch(files, [task]);
-  }
-
+gulp.task('default', ['vendor', 'server'], function() {
   compileScripts(true);
-  initWatch(htmlFiles, 'html');
-
-  gulp.watch([dist + '/**/*'], reloadPage);
+  livereload.listen({
+    port: config.livereloadPort,
+    basePath: config.distPath
+  });
+  gulp.watch([config.distPath + '/**/*'], function (evt) {
+    livereload.changed(evt.path);
+  });
+  gulp.watch('app/**/*.html', ['html']);
 });
